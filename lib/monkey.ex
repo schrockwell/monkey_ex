81a8b24ba306a1c8ed6d752f377e9f2184cb1cf9
Rockwell Schrock <rockwell@schrock.me>
2023-07-04 10:28:39 -0400

    Remove boilerplate

--- lib/monkey.ex
+++ lib/monkey.ex
@@ -2,17 +2,4 @@ defmodule Monkey do
   @moduledoc """
   Documentation for `Monkey`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Monkey.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/monkey_test.exs
+++ test/monkey_test.exs
@@ -1,8 +1,3 @@
 defmodule MonkeyTest do
   use ExUnit.Case
-  doctest Monkey
-
-  test "greets the world" do
-    assert Monkey.hello() == :world
-  end
 end
